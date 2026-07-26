49d88d8d3484efda82b56c34674e9ea66f191d0b
jerichomc <jerichomc255@gmail.com>
2026-07-26 15:47:33 -0700

    watchlist route

---
+++ backend/routes/watchlistRoutes.js
@@ -0,0 +1,39 @@
+import express from "express";
+
+const router = express.Router();
+
+let watchlist = [];
+
+router.get("/", (req, res) => {
+  res.json(watchlist);
+}); // Get all movies in the watchlist
+
+router.post("/", (req, res) => { // Add a movie to the watchlist
+  const movie = req.body;
+
+  const movieAlreadySaved = watchlist.some((savedMovie) => {
+    return savedMovie.id === movie.id;
+  });
+
+  if (movieAlreadySaved) {
+    return res.status(409).json({
+      message: "Movie is already in watchlist",
+    });
+  }
+
+  watchlist.push(movie);
+
+  res.status(201).json(movie); 
+});
+
+router.delete("/:id", (req, res) => {
+  const movieId = Number(req.params.id);
+
+  watchlist = watchlist.filter((movie) => {
+    return movie.id !== movieId;
+  });
+
+  res.status(204).send();
+});
+
+export default router;
