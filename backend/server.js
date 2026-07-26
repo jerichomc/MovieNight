552d5e5852fc26281dcda52db2c405a22c1a3960
jerichomc <jerichomc255@gmail.com>
2026-07-26 15:48:06 -0700

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import watchlistRoutes from "./routes/watchlistRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/watchlist", watchlistRoutes);
 
 app.get("/api/health", (req, res) => {
     res.json({
